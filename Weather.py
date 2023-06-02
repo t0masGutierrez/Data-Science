e3f2aef6fbd413b5b30ff4f52a6796df5c80b55b
Tomas Gutierrez <89363759+tgut5510@users.noreply.github.com>
2023-06-01 21:36:49 -0500

    Update Weather.py

--- Weather.py
+++ Weather.py
@@ -1,17 +1,20 @@
+# API Connection
 import requests
 url = "http://api.openweathermap.org/data/2.5/forecast?"
-key = "ed483c3ee33c8fb1be95422824064b36"
+key = "4b4ee3244032ef1b6bfc8333dc2c428e"
 city = "Houston"
 test = url + "appid=" + key + "&q=" + city
 
+# Temperature Conversion
 def kelvinToFahrenheit(kelvin):
   celsius = kelvin - 273.15
   fahrenheit = celsius * (9/5) + 32
   return fahrenheit
 
+# Get Temperature
 response = requests.get(test).json()
-
 tempKelvin = response['list'][0]['main']['temp']
 tempFahrenheit = kelvinToFahrenheit(tempKelvin)
 
-print(f"temperature in {city} is currently {tempFahrenheit:0.2f} degrees Fahrenheit")
+# Output
+print(f"{city} is currently {tempFahrenheit:0.2f} degrees Fahrenheit")
