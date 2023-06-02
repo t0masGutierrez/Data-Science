8646c127877040632c21fd1d0b75962afc152b32
Tomas Gutierrez <89363759+tgut5510@users.noreply.github.com>
2023-06-01 21:32:02 -0500

    Delete Random Name Generator.py

--- Random Name Generator.py
+++
@@ -1,7 +0,0 @@
-import requests
-response = requests.get("https://randomuser.me/api")
-
-firstName = response.json()['results'][0]['name']['first']
-lastName = response.json()['results'][0]['name']['last']
-
-print("Name - " + firstName + " " + lastName)
